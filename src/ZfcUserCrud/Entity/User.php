d49208e4167b8a6e601a09ee673bf80b4eaf838b
Fábio Felipe Paiva <fabio@paiva.info>
2014-11-10 16:25:21 -0200

    Update User.php

--- src/ZfcUserCrud/Entity/User.php
+++ src/ZfcUserCrud/Entity/User.php
@@ -12,6 +12,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ZfcUser\Entity\UserInterface;
+use BjyAuthorize\Provider\Role\ProviderInterface as RoleProviderInterface;
 
 /**
  * An example of how to implement a role aware user entity.
@@ -21,7 +22,7 @@ use ZfcUser\Entity\UserInterface;
  *
  * @author Tom Oram <tom@scl.co.uk>
  */
-class User implements UserInterface
+class User implements UserInterface, RoleProviderInterface
 {
     /**
      * @var int
